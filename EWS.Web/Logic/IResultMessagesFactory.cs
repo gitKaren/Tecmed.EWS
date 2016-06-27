66df7ff22193308b21df823b20d6067305bfcea0
gitKaren <karen@cws.tech>
2016-06-27 12:53:35 +0200

    Missing files

---
+++ EWS.Web/Logic/IResultMessagesFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EWS.Web.Logic
+{
+    public interface IResultMessagesFactory
+    {
+        IEnumerable<ResultMessage> GetMessages(Exception e);
+        IEnumerable<ResultMessage> GetMessages(ResultMessageType msgType);
+        IEnumerable<ResultMessage> GetMessages(ResultMessageType msgType, string operationDescription);
+        IEnumerable<ResultMessage> GetMessages(ResultMessageType msgType, string[] messages);
+    }
+}
---
+++ EWS.Web/Logic/ResultMessage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EWS.Web.Logic
+{
+    public enum ResultMessageType
+    {
+        Success,
+        Error
+    }
+
+    public class ResultMessage
+    {
+        public ResultMessage(ResultMessageType messageType, string message)
+        {
+            this.MessageType = messageType;
+            this.Message = message;
+        }
+
+        public ResultMessageType MessageType { get; private set; }
+        public string Message { get; private set; }
+    }
+}
